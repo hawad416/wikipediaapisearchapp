4b0598d236e23d5c518c173cbca7e0f2210f61a3
Hawa Drammeh <hawad416@gmail.com>
2021-12-31 03:47:53 -0800

    created httpclient dependency injection

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WikipediaService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   public search(term: string) {
-    return 'i am wikipedia search result';
+    return this.http.get('https://en.wikipedia.org/w/api.php');
   }
 }
